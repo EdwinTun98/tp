d017c7d1feb4e357ba5c11983d2f5821e27047f0
rchlai <22b6034@ubd.edu.bn>
2025-03-14 15:12:53 +0800

    Populated Storage class

--- src/main/java/seedu/duke/Storage.java
+++ src/main/java/seedu/duke/Storage.java
@@ -1,4 +1,46 @@
 package seedu.duke;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Storage {
+    private final String filePath;
+
+    public Storage(String path) {
+        this.filePath = path;
+    }
+
+    public void saveEntries(ArrayList<String> moneyList) throws MTException {
+        try (FileWriter writer = new FileWriter(filePath)) {
+            for (String entry: moneyList) {
+                writer.write(entry + "\n");
+            }
+        } catch (IOException e) {
+            throw new MTException("Error saving entries: " + e.getMessage());
+        }
+    }
+
+    public ArrayList<String> loadEntries() throws MTException {
+        ArrayList<String> entries = new ArrayList<>();
+        File file = new File(filePath);
+
+        if (!file.exists()) {
+            return entries;
+        }
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                entries.add(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            throw new MTException("File not found. Starting with an empty list.");
+        }
+
+        return entries;
+    }
 }
+
