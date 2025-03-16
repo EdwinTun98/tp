bef96bc0820284d97a9f0e9372545e4b8e481a9a
rchlai <22b6034@ubd.edu.bn>
2025-03-16 10:15:11 +0800

    Logging info in Storage class

--- src/main/java/seedu/duke/Storage.java
+++ src/main/java/seedu/duke/Storage.java
@@ -8,23 +8,30 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Storage {
+    private final MTLogger logger;
     private final String filePath;
 
     public Storage(String path) {
         this.filePath = path;
+        this.logger = new MTLogger(Storage.class.getName());
     }
 
     public void saveEntries(ArrayList<String> moneyList) throws MTException {
+        logger.logInfo("Saving entries into " + filePath);
+
         try (FileWriter writer = new FileWriter(filePath)) {
             for (String entry: moneyList) {
                 writer.write(entry + "\n");
             }
-        } catch (IOException e) {
-            throw new MTException("Error saving entries: " + e.getMessage());
+        } catch (IOException error) {
+            logger.logSevere("Error saving entries into " + filePath, error);
+            throw new MTException("Error saving entries: " + error.getMessage());
         }
     }
 
     public ArrayList<String> loadEntries() throws MTException {
+        logger.logInfo("Loading previous entries from " + filePath);
+
         ArrayList<String> entries = new ArrayList<>();
         File file = new File(filePath);
 
@@ -36,7 +43,8 @@ public class Storage {
             while (scanner.hasNextLine()) {
                 entries.add(scanner.nextLine());
             }
-        } catch (FileNotFoundException e) {
+        } catch (FileNotFoundException error) {
+            logger.logSevere("Failed to find file at " + filePath, error);
             throw new MTException("File not found. Starting with an empty list.");
         }
 
