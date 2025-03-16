f4d4dfa6abfcf2b498b31aa231dd478ff722d5c2
rchlai <22b6034@ubd.edu.bn>
2025-03-16 08:07:14 +0800

    Transferred UI concerns from MoneyTrail class to TextUI class

--- src/main/java/seedu/duke/TextUI.java
+++ src/main/java/seedu/duke/TextUI.java
@@ -1,4 +1,42 @@
 package seedu.duke;
 
 public class TextUI {
+    private static final String LINE_DIVIDER = "----------------------" +
+            "-----------------------------------";
+
+    public void print(String str) {
+        System.out.println(str);
+    }
+
+    public void addLineDivider() {
+        print(LINE_DIVIDER);
+    }
+
+    public void printAppLogo() {
+        String logo = " __  __ _______ \n"
+                + "|  \\/  |__  __|\n"
+                + "| \\  / |  | |  \n"
+                + "| |\\/| |  | |  \n"
+                + "| |  | |  | |  \n"
+                + "|_|  |_|  |_|  \n";
+        print("Welcome user to\n" + logo);
+    }
+
+    public void printWelcomeMsg() {
+        printAppLogo();
+        print("What do you want to do today?");
+    }
+
+    public void printExitMsg() {
+        addLineDivider();
+        print("Exiting program... Thank you for using MoneyTrail! :)");
+    }
+
+    public void printErrorMsg(MTException error) {
+        print(error.getMessage());
+    }
+
+    public void printNumItems(int numItems) {
+        print("You now have " + numItems + " entries.");
+    }
 }
