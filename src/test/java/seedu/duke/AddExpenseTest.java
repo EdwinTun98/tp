715e24796e561bf5e32d96088769290e7357eb59
Hansel-K <e1122886@u.nus.edu>
2025-03-24 10:35:01 +0800

    Fixed code to pass pull request checks

--- src/test/java/seedu/duke/AddExpenseTest.java
+++ src/test/java/seedu/duke/AddExpenseTest.java
@@ -1,8 +1,13 @@
 package seedu.duke;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class AddExpenseTest {
     private MoneyList moneyList;
